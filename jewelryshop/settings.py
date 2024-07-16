282bf88877b3315648c43d60edf2304515c541a2
chieen-teadao <99234025+chieen-teadao@users.noreply.github.com>
2024-07-16 10:01:37 +0700

    remove sqlite, add mysql, new verision lib

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -1,5 +1,3 @@
-
-
 from pathlib import Path
 
 import os
@@ -12,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0'
+SECRET_KEY = "django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -23,75 +21,78 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.humanize',
-    'store',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.humanize",
+    "store",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'jewelryshop.urls'
+ROOT_URLCONF = "jewelryshop.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'store.context_preprocessors.store_menu',
-                'store.context_preprocessors.cart_menu',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "store.context_preprocessors.store_menu",
+                "store.context_preprocessors.cart_menu",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'jewelryshop.wsgi.application'
+WSGI_APPLICATION = "jewelryshop.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "kidustore",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -99,9 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "vi"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Asia/Bangkok"
 
 USE_I18N = True
 
@@ -113,17 +114,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jewelryshop/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Automatically Created on Production
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "jewelryshop/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")  # Automatically Created on Production
 
 # Settings for Media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- store/models.py
+++ store/models.py
@@ -2,89 +2,115 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS_CHOICES = (
+    ("Pending", "Chờ xử lý"),
+    ("Accepted", "Đã chấp nhận"),
+    ("Packed", "Đã đóng gói"),
+    ("On The Way", "Đang vận chuyển"),
+    ("Delivered", "Đã giao hàng"),
+    ("Cancelled", "Đã hủy"),
+)
+
+
+class TimeBaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Ngày tạo")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Ngày cập nhật")
+
+    class Meta:
+        abstract = True
+
+
+class StatusBaseModel(models.Model):
+    is_active = models.BooleanField(verbose_name="Hoạt động?")
+    is_featured = models.BooleanField(verbose_name="Nổi bật?")
+
+    class Meta:
+        abstract = True
+
+
 class Address(models.Model):
     user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
     locality = models.CharField(max_length=150, verbose_name="Nearest Location")
     city = models.CharField(max_length=150, verbose_name="City")
     state = models.CharField(max_length=150, verbose_name="State")
 
+    class Meta:
+        verbose_name_plural = "Địa chỉ"
+
     def __str__(self):
         return self.locality
 
 
-class Category(models.Model):
-    title = models.CharField(max_length=50, verbose_name="Category Title")
-    slug = models.SlugField(max_length=55, verbose_name="Category Slug")
-    description = models.TextField(blank=True, verbose_name="Category Description")
-    category_image = models.ImageField(upload_to='category', blank=True, null=True, verbose_name="Category Image")
-    is_active = models.BooleanField(verbose_name="Is Active?")
-    is_featured = models.BooleanField(verbose_name="Is Featured?")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+class Category(TimeBaseModel, StatusBaseModel):
+    title = models.CharField(max_length=50, verbose_name="Tiêu đề")
+    slug = models.SlugField(max_length=55, verbose_name="Slug")
+    description = models.TextField(blank=True, verbose_name="Mô tả")
+    category_image = models.ImageField(
+        upload_to="category", blank=True, null=True, verbose_name="Ảnh"
+    )
 
     class Meta:
-        verbose_name_plural = 'Categories'
-        ordering = ('-created_at', )
+        verbose_name_plural = "Danh mục"
+        ordering = ("-created_at",)
 
     def __str__(self):
         return self.title
 
 
-class Product(models.Model):
-    title = models.CharField(max_length=150, verbose_name="Product Title")
-    slug = models.SlugField(max_length=160, verbose_name="Product Slug")
-    sku = models.CharField(max_length=255, unique=True, verbose_name="Unique Product ID (SKU)")
-    short_description = models.TextField(verbose_name="Short Description")
-    detail_description = models.TextField(blank=True, null=True, verbose_name="Detail Description")
-    product_image = models.ImageField(upload_to='product', blank=True, null=True, verbose_name="Product Image")
+class Product(TimeBaseModel, StatusBaseModel):
+    title = models.CharField(max_length=150, verbose_name="Tiêu đề")
+    slug = models.SlugField(max_length=160, verbose_name="Slug")
+    sku = models.CharField(max_length=255, unique=True, verbose_name="SKU")
+    short_description = models.TextField(verbose_name="Mô tả ngắn gọn")
+    detail_description = models.TextField(
+        blank=True, null=True, verbose_name="Mô tả chi tiết"
+    )
+    product_image = models.ImageField(
+        upload_to="product", blank=True, null=True, verbose_name="Ảnh"
+    )
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    category = models.ForeignKey(Category, verbose_name="Product Categoy", on_delete=models.CASCADE)
-    is_active = models.BooleanField(verbose_name="Is Active?")
-    is_featured = models.BooleanField(verbose_name="Is Featured?")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
-
+    category = models.ForeignKey(
+        Category, verbose_name="Danh mục sản phẩm", on_delete=models.CASCADE
+    )
+    view = models.IntegerField(default=0, verbose_name="Lượt xem sản phẩm")
     class Meta:
-        verbose_name_plural = 'Products'
-        ordering = ('-created_at', )
+        verbose_name_plural = "Sản phẩm"
+        ordering = ("-created_at",)
 
     def __str__(self):
         return self.title
 
 
-class Cart(models.Model):
-    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1, verbose_name="Quantity")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated Date")
+class Cart(TimeBaseModel):
+    user = models.ForeignKey(User, verbose_name="Người dùng", on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, verbose_name="Sản phẩm", on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField(default=1, verbose_name="Số lượng")
 
     def __str__(self):
         return str(self.user)
-    
+
+    class Meta:
+        verbose_name_plural = "Giỏ hàng"
+
     # Creating Model Property to calculate Quantity x Price
     @property
     def total_price(self):
         return self.quantity * self.product.price
 
 
-STATUS_CHOICES = (
-    ('Pending', 'Pending'),
-    ('Accepted', 'Accepted'),
-    ('Packed', 'Packed'),
-    ('On The Way', 'On The Way'),
-    ('Delivered', 'Delivered'),
-    ('Cancelled', 'Cancelled')
-)
-
 class Order(models.Model):
-    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
-    address = models.ForeignKey(Address, verbose_name="Shipping Address", on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(verbose_name="Quantity")
-    ordered_date = models.DateTimeField(auto_now_add=True, verbose_name="Ordered Date")
-    status = models.CharField(
-        choices=STATUS_CHOICES,
-        max_length=50,
-        default="Pending"
-        )
+    user = models.ForeignKey(User, verbose_name="Người dùng", on_delete=models.CASCADE)
+    address = models.ForeignKey(
+        Address, verbose_name="Địa chỉ", on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product, verbose_name="Sản phẩm", on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField(verbose_name="Số lượng")
+    ordered_date = models.DateTimeField(auto_now_add=True, verbose_name="Ngày đặt hàng")
+    status = models.CharField(choices=STATUS_CHOICES, max_length=50, default="Pending")
+
+    class Meta:
+        verbose_name_plural = "Đơn hàng"
